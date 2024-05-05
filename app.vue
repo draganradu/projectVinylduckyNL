e72920fe0a3eb652b6f93270ffc939b944087d07
Radu Dragan <radudragan@fotodex.ro>
2024-05-05 21:31:46 +0200

    ECBGI-102 Added bootrsap

--- app.vue
+++ app.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <div class="container">
+      <div class="row">
+        <div class="col-sm">
+          1
+        </div>
+        <div class="col-sm">
+          2
+        </div>
+        <div class="col-sm">
+          3
+        </div>
+      </div>
+    </div>
   </div>
 </template>
