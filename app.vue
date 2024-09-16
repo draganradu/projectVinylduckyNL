f15ac861faefef00811727b6ddd9157f81560e15
Radu Dragan <radudragan@fotodex.ro>
2024-09-16 11:08:16 +0200

    fixed modal issue

--- app.vue
+++ app.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
 const store = useStore();
-const { modalOpen } = ref(store.modes); // toDo:ECBGI-117
 </script>
 
 <template>
-  <Modal v-if="modalOpen" />
-  <div :class="modalOpen ? 'modal-in-view' : 'no-modal-in-view'">
+  <Modal v-if="store.modes.modalOpen" />
+  <div :class="store.modes.modalOpen ? 'modal-in-view' : 'no-modal-in-view'">
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
